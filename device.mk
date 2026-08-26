3b7795b38f5676d473ebd12669da327e2b4b481a
Jonas Salo <salomannen1234@gmail.com>
2026-08-27 00:17:37 +0200

    camry: fix AIDL security service trigger

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_PACKAGES += \
     bootctrl.oneplus_sm6375.recovery \
-    android.hardware.boot@1.2-impl-qti.recovery
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.health-V2-ndk
 
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL := 34
